f216ee91a4d4661d8a124bf248c56a5e206d3406
Abdul Rehman <hafizrehman925@gmail.com>
2023-12-30 17:17:59 +0500

    counter

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,20 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
   <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <NumberCounter :count="1" />
+  <CounterNumber />
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import NumberCounter  from './components/test.vue'
+import CounterNumber  from './components/counter.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    NumberCounter,
+    CounterNumber,
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
